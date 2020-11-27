fcba358e6c073fb75b20ce7256ad9aeb08e6f64b
Tyler <tuh26432@temple.edu>
2020-11-27 16:44:39 -0500

    Chart JS (WORKED ON AS A GROUP )

--- book_soundtrack_creator/static/js/book_stats.js
+++ book_soundtrack_creator/static/js/book_stats.js
@@ -1,13 +1,71 @@
-let url = 
+var bookID = window.location.href.split('/')[4]
+console.log(bookID)
+let url = "http://127.0.0.1:8000/get_book/"+bookID
+let bookEmotionData = {}
+let bookEmotionNew = []
+getBook(url)
+console.log(getBook(url))
+console.log(document.getElementById('chart'))
+var ctx = document.getElementById('chart').getContext('2d');
+
 async function getBook(url){
     let book;
-    console.log(url)
     const resp =  await fetch(url);
     respBook = await resp.json() ;
-    book = respBook
-    console.log(book)
-
-    importBook(book)
+    bookEmotion = respBook['emotionDict']
+    // console.log(bookEmotion)
+    
+    for (var emo in bookEmotion){
+        // console.log(bookEmotion[emo])
+        bookEmotionNew.push(bookEmotion[emo])
+    }
+    console.log(bookEmotionNew)
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['anticipation', 'fear', 'anger', 'trust', 'surprise', 'positive', 'negative', 'sadness', 'disgust', 'joy'],
+            datasets: [{
+                label: '# of Votes',
+                data: bookEmotionNew,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
 }
 
 // The following function are copying from 
